dd728df71a1721859ef304121d7b907319b82edf
hwpwk <34832156+hwpwk@users.noreply.github.com>
2018-09-10 21:48:28 +0900

    Add files via upload

--- 180910_taitanic.R
+++ 180910_taitanic.R
@@ -80,7 +80,75 @@ sum(is.na(df_train2))
 sum(is.na(df_test2))
 # どちらも0なので補完完了
 plot(df_train2)
-# Survivedと他の変数との関係が0or1で直線的な相関が見えにくい、、シグモイド関数的な相関関係
+# Survivedと他の変数との関係が0or1で直線的な相関が見えにくい、、シグモイド関数的な相関関係？
 
 cor(df_train2)
 # Suviviedと相関が強いのは順にsex:0.54, Pclass:0.33, Fare:0.25
+
+# モデル構築
+# 今回はランダムフォレストを選択
+install.packages('randomForest')
+library(randomForest)
+
+# The response has five or fewer unique values.というエラー
+# 「予測結果の値が5種類以下しか無いが、本当にregressionがしたいのか? ( classification がしたいのでは? )」という意味
+# 目的変数がlogical型になっていると、regression モード になるっぽい。 
+# 目的変数をfactor型に変換してあげればclassification モードになる。
+# 参考:http://shinya131-note.hatenablog.jp/entry/2015/10/26/233823
+df_train2$Survived <- as.factor(df_train2$Survived)
+
+model <- randomForest(Survived~.,
+                      data = df_train2)
+model
+# OOB estimate of  error rate: 17.96%
+
+
+
+# 特徴量毎の重要度を表示
+importance(model)
+# 複数行を1度にコメントアウトするには コメント箇所をドラッグしCTRL + SHIFT + C
+# MeanDecreaseGini
+# PassengerId         48.90437
+# Pclass              33.20005
+# Sex                 98.02156
+# Age                 49.70134
+# SibSp               15.60726
+# Parch               12.64400
+# Fare                64.11726
+# Embarked            10.86850
+
+# 特徴量の重要度をプロット
+varImpPlot(model)
+
+
+# 重要度が高い特徴量のみでランダムフォレストをやってみる
+model2 <- randomForest(Survived~Sex+Age+Fare,
+                       data = df_train2)
+model2
+# OOB estimate of  error rate: 20.76%
+
+importance(model2)
+# MeanDecreaseGini
+# Sex         106.82327
+# Age          30.50964
+# Fare         56.67634
+
+# 精度が良いmodelの方をテストデータに当てはめる
+pred <- predict(model, newdata = df_test2)
+
+head(pred, 10)
+# このときpredは0or1
+
+# cbindにするとなぜか1,2データになる,,
+# output <- cbind(df_test2$PassengerId, pred)
+# colnames(output) <- c('PassengerId', 'Survived')
+# data.frameにすると0,1データになった
+# data.frameにする際にはカラム名を指定する
+# http://zeema.hatenablog.com/entry/2017/09/04/003400#%E3%83%AD%E3%82%B8%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E5%9B%9E%E5%B8%B0%E3%83%A2%E3%83%87%E3%83%AB%E3%81%AE%E6%8E%A8%E5%AE%9A1%E5%9B%9E%E7%9B%AE
+output1 <- data.frame(PassengerId=df_test2$PassengerId, Survived=pred)
+
+# csvファイルとして出力
+# row.names=FALSEってしておかないと、行列の行番号がそのままCSVの1列目のデータとして出力されてしまう
+write.csv(output1, 'submission_1.csv', row.names=FALSE)
+
+# scoreは0.74641
